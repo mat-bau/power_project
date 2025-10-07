6c6b22a5eb4e6f0be285a27ec4a4164472c0d5a6
Guillaume Tilmans <guillaume.tilmans@student.uclouvain.be>
2025-10-07 17:08:06 +0200

    Tilmans création bus

--- project.py
+++ project.py
@@ -21,17 +21,28 @@ def My_Amazing_Grid():
 
     # Grid 1 - Tilmans
 
+    B27 = pp.create_bus(net, vn_kv=20, name="B1")
+    B28 = pp.create_bus(net, vn_kv=380, name="B2")
+    B29 = pp.create_bus(net, vn_kv=150, name="B3")
+    B30 = pp.create_bus(net, vn_kv=15, name="B4")
+    B31 = pp.create_bus(net, vn_kv=380, name="B5")
+    B32 = pp.create_bus(net, vn_kv=20, name="B6")
+    B33 = pp.create_bus(net, vn_kv=150, name="B7")
+    B34 = pp.create_bus(net, vn_kv=20, name="B8")
+
+
 
     # Grid 2 - Scupp
+
 
 
     # Grid 3 - Bauvir
 
 
-    # Grid 4 - Tilmans
+    #  Grid 4 - Tilmans
 
 
-    # Grid 5 - Schupp
+    #   Grid 5 - Schupp
 
 
     return net
